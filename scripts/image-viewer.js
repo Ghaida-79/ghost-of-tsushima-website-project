8d4a3b855272ebf8eeba1870b81753b809d4056c
Ghaida <mo0on7gogo@gmail.com>
2026-01-05 08:36:20 +0300

    Fix modal display, image sizing, and download button.

--- scripts/image-viewer.js
+++ scripts/image-viewer.js
@@ -6,34 +6,33 @@ let nextButton = document.querySelector('.next-button');
 let previousButton = document.querySelector('.previous-button');
 let closeButton = document.querySelector('.close-button');
 
+let downloadButton = document.querySelector('.box-download');
+
 let index = 0;
 
 images.forEach((img, i) => {
   img.onclick = () => {
     index = i;
     boxImage.src = img.src;
+    downloadButton.href = img.dataset.full;
     box.style.display = 'flex';
   };
 });
 
 nextButton.onclick = () => {
   index++;
-
-  if (index == images.length) {
-    index = 0;
-  }
+  if (index == images.length) index = 0;
 
   boxImage.src = images[index].src;
+  downloadButton.href = images[index].dataset.full;
 };
 
 previousButton.onclick = () => {
   index--;
-
-  if (index < 0) {
-    index = images.length - 1;
-  }
+  if (index < 0) index = images.length - 1;
 
   boxImage.src = images[index].src;
+  downloadButton.href = images[index].dataset.full;
 };
 
 closeButton.onclick = () => {
--- scripts/wallpapers.js
+++ scripts/wallpapers.js
@@ -1,23 +1,44 @@
-const wallpapers = [
-  'wallpapers/wallpaper1.jpg',
-  'wallpapers/wallpaper2.jpg',
-  'wallpapers/wallpaper3.jpg',
-  'wallpapers/wallpaper4.jpg',
-  'wallpapers/wallpaper5.jpg',
-  'wallpapers/wallpaper6.jpg',
-  'wallpapers/wallpaper7.jpg',
-  'wallpapers/wallpaper8.jpg'
+const wallpapers = [{
+  preview: 'wallpapers/previews/wallpaper1.jpg',
+  full: 'wallpapers/full/wallpaper1.jpg'
+}, {
+  preview: 'wallpapers/previews/wallpaper2.jpg',
+  full: 'wallpapers/full/wallpaper2.jpg'
+}, {
+  preview: 'wallpapers/previews/wallpaper3.jpg',
+  full: 'wallpapers/full/wallpaper3.jpg'
+}, {
+  preview: 'wallpapers/previews/wallpaper4.jpg',
+  full: 'wallpapers/full/wallpaper4.jpg'
+}, {
+  preview: 'wallpapers/previews/wallpaper5.jpg',
+  full: 'wallpapers/full/wallpaper5.jpg'
+}, {
+  preview: 'wallpapers/previews/wallpaper6.jpg',
+  full: 'wallpapers/full/wallpaper6.jpg'
+}, {
+  preview: 'wallpapers/previews/wallpaper7.jpg',
+  full: 'wallpapers/full/wallpaper7.jpg'
+}, {
+  preview: 'wallpapers/previews/wallpaper8.jpg',
+  full: 'wallpapers/full/wallpaper8.jpg'
+}
 ];
 
 let wallpapersHTML = '';
 
 wallpapers.forEach((wallpaper, index) => {
   wallpapersHTML += `
+  <div class="">
     <img 
-      src="${wallpaper}"
+      src="${wallpaper.preview}"
+      data-full="${wallpaper.full}"
       class="shot"
       loading="lazy"
-      alt="Wallpaper ${index + 1}">
+      alt="Wallpaper ${index + 1}"
+    >
+  </div>
+    
   `;
 });
 
